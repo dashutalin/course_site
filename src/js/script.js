ea3236356856ba9caec022f19db99f9d1f01967d
Dasha <dasha-v-l@yandex.ru>
2022-03-14 20:51:32 +0300

    create third screen and script.js

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrows/left.svg"></button>', 
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrows/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
+      });
+  });
